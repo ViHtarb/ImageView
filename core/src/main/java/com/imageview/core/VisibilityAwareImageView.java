426aebdc3cb7f4fe2983bf12e629889174b81e20
Viнt@rь <vihtarb@gmail.com>
2018-07-31 12:00:17 +0300

    Fixed app crash on android API 21 with using backgroundTint

--- core/src/main/java/com/imageview/core/VisibilityAwareImageView.java
+++ core/src/main/java/com/imageview/core/VisibilityAwareImageView.java
@@ -1,13 +1,31 @@
+/*
+ * Copyright (C) 2015 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.imageview.core;
 
+import android.annotation.SuppressLint;
 import android.content.Context;
-import android.support.v7.widget.AppCompatImageView;
 import android.util.AttributeSet;
+import android.widget.ImageView;
 
 /**
  * Created by Viнt@rь on 01.09.2017
  */
-class VisibilityAwareImageView extends AppCompatImageView {
+@SuppressLint("AppCompatCustomView")
+class VisibilityAwareImageView extends ImageView {
 
     private int mUserSetVisibility;
 
